d5e9ee0c7af8439913d03ad7e94c19d750634b73
Anurag Kandulna <dev.ak@outlook.in>
2024-08-04 20:29:10 +0530

    fixed issues

--- parser.py
+++ parser.py
@@ -106,7 +106,7 @@ def transcribe_audio(audio_file_name, audio_file_path, duration_sec=300):
     # Transcribe the audio file
     with sr.AudioFile(temp_file_name) as source:
         audio_text = r.record(source)
-        
+
         try:
             # Using Google speech recognition to transcribe in Hindi
             text = r.recognize_google(audio_text, language="hi-IN")
@@ -173,9 +173,12 @@ if __name__ == '__main__':
         segmented_audio_files = get_all_audio_files_in_dir(audio_file_dir=audio_tuple[2])
 
         for seg_fname, seg_fpath in segmented_audio_files:
+            print(f'Starting transcribing {seg_fpath}...')
             transcription = transcribe_audio(audio_file_name=seg_fname, audio_file_path=seg_fpath, duration_sec=300)
 
+            print(f'Starting writing to file for audio {seg_fname}...')
             write_text_to_file(text_data=transcription, audio_file_name=seg_fname, audio_file_path=seg_fpath, dest_text_dir=audio_tuple[3])
+            break;
 
     # audio_file_path = '/Users/anurag/Projects/audio-parser/output/Timothy_1/Timothy_1_part_1.mp3'
     # transcription = transcribe_audio(file_path=audio_file_path, duration_sec=300)
