bfc744f738b35d7630309ec2b0c11277000b156b
Anurag Kandulna <dev.ak@outlook.in>
2024-08-05 00:49:43 +0530

    custom logger script

---
+++ src/custom_logger.py
@@ -0,0 +1,31 @@
+"""Custom logger script."""
+
+import logging
+
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create handlers
+file_handler = logging.FileHandler('app_parser.log')
+console_handler = logging.StreamHandler()
+
+# Set level of logging for handlers
+file_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.DEBUG)
+
+# Create formatters and add them to handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# You can also add a function to get the logger
+def get_logger(name=__name__):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    return logger
