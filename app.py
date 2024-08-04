a7f10fc182c1f3fffb80aa51b64ac76b3536fb01
Anurag Kandulna <dev.ak@outlook.in>
2024-08-05 02:12:31 +0530

    optimized imports on app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import time
 import os
-import speech_recognition as sr
-from pydub import AudioSegment
+import src.parser
+from src.custom_logger import get_logger
+
+
+# Invoke Logger
+LOGGER = get_logger()
+
 
 ##################################################################
 if __name__ == '__main__':
@@ -22,10 +27,8 @@ if __name__ == '__main__':
         segmented_audio_files = get_all_audio_files_in_dir(audio_file_dir=audio_tuple[2])
 
         for seg_fname, seg_fpath in segmented_audio_files:
-            print(f'Starting transcribing {seg_fpath}...')
+            # Get transcription for audio segment
             transcription = transcribe_audio(audio_file_name=seg_fname, audio_file_path=seg_fpath, duration_sec=300)
 
-            print(f'Starting writing to file for audio {seg_fname}...')
+            # Write transcribed text to file
             write_text_to_file(text_data=transcription, audio_file_name=seg_fname, audio_file_path=seg_fpath, dest_text_dir=audio_tuple[3])
-            break
-  
